1147866dc03d924db88d87b8350a167fc1e5d062
David Urbina <davidurbina.dev@gmail.com>
2023-12-03 15:16:05 -0500

    Ignore `target/` directory

--- target/debug/backup.d
+++
@@ -1,1 +0,0 @@
-/mnt/drive/dev/shell-scripts/modules/backup/target/debug/backup: /mnt/drive/dev/shell-scripts/modules/backup/src/main.rs
--- target/debug/deps/backup-1289a325b94ab487.d
+++
@@ -1,5 +0,0 @@
-/mnt/drive/dev/shell-scripts/modules/backup/target/debug/deps/backup-1289a325b94ab487: src/main.rs
-
-/mnt/drive/dev/shell-scripts/modules/backup/target/debug/deps/backup-1289a325b94ab487.d: src/main.rs
-
-src/main.rs:
